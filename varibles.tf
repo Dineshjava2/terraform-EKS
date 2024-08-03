98d88b377d21141f3d8916a090c591e96702c207
dinesh <dinesh.kantubukta@gmail.com>
2024-08-03 18:29:59 +0530

    modificaation in vpc file

---
+++ varibles.tf
@@ -0,0 +1,25 @@
+variable "kubernetes_version" {
+  default     = 1.29
+  description = "kubernetes version"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "aws_region" {
+  default = "ap-south-1"
+  description = "aws region"
+}
+
+variable "cluster_name" {
+  default = "dine-cluster"
+  description = "this is cluster name"
+}
+variable "cidr_blocks" {
+  default = [""10.0.0.0/8","172.16.0.0/12","192.168.0.0/16","]
+}
+
+variable "" {
+  
+}
--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,7 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
-
-  name                 = "abhi-eks-vpc"
+  version = "5.10.0"
+  name                 = "dine-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -28,16 +12,16 @@ module "vpc" {
   enable_dns_support   = true
 
   tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
